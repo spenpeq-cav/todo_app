70cbd57170611accdcd0a437d2ab738d06e8757e
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-09 17:05:04 -0800

    Added templates, create model (Task), model form, render data in template.

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+from .forms import *
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+    context = {'tasks': tasks , 'form':form}
+    return render(request, 'tasks/list.html', context)
